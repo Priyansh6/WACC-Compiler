d877390945eed3dc8d206040246e37fb59a73762
Seyhan Khan <seyhan.khan21@imperial.ac.uk>
2023-02-09 20:33:46 +0000

    feat: display syntax errors

--- app/Main.hs
+++ app/Main.hs
@@ -14,6 +14,7 @@ import System.Exit
 import qualified Data.Map as M
 import qualified Data.Text.IO as TIO
 import qualified Lexer as L
+import SemanticErrors
 
 main :: IO ()
 main = do 
@@ -21,9 +22,11 @@ main = do
   contents <- TIO.readFile fname
   let res = runParser (L.fully pProgram) fname contents
   case res of
-    Left _ -> exitWith (ExitFailure 100)
+    Left err -> do
+      putStrLn (errorBundlePretty err)
+      exitWith (ExitFailure 100)
     Right ast -> case rename ast of
       ((_, []), renamedAST) -> case runExcept $ runStateT (checkProg renamedAST) M.empty of
         Left err -> print err >> exitWith (ExitFailure 200)
         Right _ -> exitSuccess
-      ((_, errs), _) -> print errs >> exitWith (ExitFailure 200)
+      ((_, errs), _) -> printSemanticErrors contents errs >> exitWith (ExitFailure 200)
