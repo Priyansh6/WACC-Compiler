d1df419e9f610b7ce4dc5675ec55d6eaa8ac6205
Hamish McCreanor <hmccreanor1@tribeig.com>
2023-02-10 16:51:11 +0000

    feat: added labels to parser smart constructors for better syntax error messages

--- src/parse/ExpressionConstructors.hs
+++ src/parse/ExpressionConstructors.hs
@@ -12,34 +12,35 @@ module ExpressionConstructors
 where
 
 import Control.Applicative ((<**>))
-import Parser (Parser, liftPos1, liftPos2, deferLiftPos1, deferLiftPos2, getPosition)
+import Parser (Parser, liftPos1, liftPos2, getPosition)
+import Text.Megaparsec (label)
 
 import qualified AST
 import qualified Data.Text as T
 
 mkIdent :: Parser T.Text -> Parser AST.Ident
-mkIdent = liftPos1 AST.Ident 
+mkIdent = label "identifier" . liftPos1 AST.Ident 
 
 mkArrayElem :: Parser AST.Ident -> Parser [AST.Expr] -> Parser AST.ArrayElem
-mkArrayElem = liftPos2 AST.ArrayElem 
+mkArrayElem = (label "array expression" .) . liftPos2 AST.ArrayElem
 
 mkIdentExpr :: Parser AST.Ident -> Parser AST.Expr
-mkIdentExpr = liftPos1 AST.IdentExpr 
+mkIdentExpr = label "identifier" . liftPos1 AST.IdentExpr 
 
 mkArrayExpr :: Parser AST.ArrayElem -> Parser AST.Expr
-mkArrayExpr = liftPos1 AST.ArrayExpr 
+mkArrayExpr = label "array expression" . liftPos1 AST.ArrayExpr 
 
 mkBool :: Parser Bool -> Parser AST.Expr
-mkBool = liftPos1 AST.BoolLiter
+mkBool = label "boolean literal" . liftPos1 AST.BoolLiter
 
 mkInt :: Parser Integer -> Parser AST.Expr
-mkInt = liftPos1 AST.IntLiter 
+mkInt = label "int literal" . liftPos1 AST.IntLiter 
 
 mkChar :: Parser Char -> Parser AST.Expr
-mkChar = liftPos1 AST.CharLiter 
+mkChar = label "char literal" . liftPos1 AST.CharLiter 
 
 mkString :: Parser T.Text -> Parser AST.Expr
-mkString = liftPos1 AST.StrLiter 
+mkString = label "string literal" . liftPos1 AST.StrLiter 
 
 mkPairLiter :: Parser () -> Parser AST.Expr
-mkPairLiter p = getPosition <**> (AST.PairLiter <$ p)
+mkPairLiter p = label "null" getPosition <**> (AST.PairLiter <$ p)
--- src/parse/StatementConstructors.hs
+++ src/parse/StatementConstructors.hs
@@ -15,42 +15,42 @@ module StatementConstructors
 where
 
 import Parser (Parser, liftPos1, liftPos2, liftPos3)
-import Text.Megaparsec ((<|>))
+import Text.Megaparsec ((<|>), (<?>), label)
 
 import qualified AST
 
 mkDecAssign :: Parser AST.WType -> Parser AST.Ident -> Parser AST.RVal -> Parser AST.Stat
-mkDecAssign = liftPos3 AST.DecAssign 
+mkDecAssign t i rval = label "variable declaration and assignment" $ liftPos3 AST.DecAssign t i rval
 
 mkAssign :: Parser AST.LVal -> Parser AST.RVal -> Parser AST.Stat
-mkAssign = liftPos2 AST.Assign 
+mkAssign = (label "variable assignment" .) . liftPos2 AST.Assign 
 
 mkRead :: Parser AST.LVal -> Parser AST.Stat
-mkRead = liftPos1 AST.Read 
+mkRead = label "read statement" . liftPos1 AST.Read 
 
 mkArrLiter :: Parser [AST.Expr] -> Parser AST.RVal 
-mkArrLiter = liftPos1 AST.ArrayLiter 
+mkArrLiter = label "array literal" . liftPos1 AST.ArrayLiter 
 
 mkNewPair :: Parser AST.Expr -> Parser AST.Expr -> Parser AST.RVal
-mkNewPair = liftPos2 AST.NewPair 
+mkNewPair = (label "newpair statement" .) . liftPos2 AST.NewPair 
 
 mkPairElem :: Parser AST.LVal -> Parser AST.LVal -> Parser AST.PairElem
-mkPairElem l r = liftPos1 AST.Fst l <|> liftPos1 AST.Snd r
+mkPairElem l r = label "fst or snd statement" $ liftPos1 AST.Fst l <|> liftPos1 AST.Snd r
 
 mkCall :: Parser AST.Ident -> Parser [AST.Expr] -> Parser AST.RVal
-mkCall = liftPos2 AST.Call 
+mkCall = (label "call statement" .) . liftPos2 AST.Call 
 
 mkFree :: Parser AST.Expr -> Parser AST.Stat
-mkFree = liftPos1 AST.Free 
+mkFree = label "free statement" . liftPos1 AST.Free 
 
 mkReturn :: Parser AST.Expr -> Parser AST.Stat 
-mkReturn = liftPos1 AST.Return 
+mkReturn = label "return statement" . liftPos1 AST.Return 
 
 mkExit :: Parser AST.Expr -> Parser AST.Stat
-mkExit = liftPos1 AST.Exit 
+mkExit = label "exit statement" . liftPos1 AST.Exit 
 
 mkIf :: Parser AST.Expr -> Parser AST.Stats -> Parser AST.Stats -> Parser AST.Stat
-mkIf = liftPos3 AST.If 
+mkIf c b1 b2 = label "if statement" $ liftPos3 AST.If (c <?> "condition") (b1 <?> "branch") (b2 <?> "branch")
 
 mkWhile :: Parser AST.Expr -> Parser AST.Stats -> Parser AST.Stat
-mkWhile = liftPos2 AST.While
+mkWhile c b = label "while statement" $ liftPos2 AST.While (c <?> "condition") (b <?> "body")
