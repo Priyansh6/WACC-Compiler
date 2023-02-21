4398c309e69e9f2df2a5cc86967154e7225ec9ef
Hamish McCreanor <hmccreanor1@tribeig.com>
2023-02-21 10:14:20 +0000

    feat: added generic special registers (PC, FP, etc) to IRReg datatype

--- src/codeGeneration/IR.hs
+++ src/codeGeneration/IR.hs
@@ -6,8 +6,8 @@ where
 
 import qualified Data.Text as T
 
-data IRReg = TmpReg Int 
-data ArmReg = R0 | R1 | R2 | R3 | R4 | R5 | R6 | R7 | R8 | R9 | R10 | R11 | R12 | SP | LR | PC
+data IRReg = TmpReg Int | IRFP | IRSP | IRLR | IRPC 
+data ArmReg = R0 | R1 | R2 | R3 | R4 | R5 | R6 | R7 | R8 | R9 | R10 | FP | R12 | SP | LR | PC
 
 type ArmInstr = Instr ArmReg
 type IRInstr = Instr IRReg
