29a95087309090f1d2a35840d7617c53891a50c6
Seyhan Khan <seyhan.khan21@imperial.ac.uk>
2023-02-22 13:05:55 +0000

    fix: export all relevant types from IR

--- src/CodeGeneration/IR.hs
+++ src/CodeGeneration/IR.hs
@@ -1,52 +1,48 @@
-module CodeGeneration.IR 
-  ( Instrs, 
-    Instr,
-    IRReg,
-    IRInstr,
-    IRInstrs,
-    Ident,
-    FPOffsets
-  )
-where
+module CodeGeneration.IR (module CodeGeneration.IR) where
 
 import qualified Data.Map as M
 import qualified Data.Text as T
 
 type Program a = [Section a]
+
 data Section a = Section Label [Data] [Instr a]
+
 data Data = StringData Label T.Text
 
-data IRReg = TmpReg Int | IRFP | IRSP | IRLR | IRPC 
+data IRReg = TmpReg Int | FP | SP | LR | PC
 
 type Instrs a = [Instr a]
+
 type IRInstrs = Instrs IRReg
 
 data Ident = Ident T.Text
 
-data Instr a = Load (Operand a) (Operand a)
-             | Store (Operand a) (Operand a)
-             | Mov (Operand a) (Operand a)
-             | Add (Operand a) (Operand a) (Operand a)
-             | Sub (Operand a) (Operand a) (Operand a)
-             | Mul (Operand a) (Operand a) (Operand a)
-             | Div (Operand a) (Operand a) (Operand a)
-             | Cmp (Operand a) (Operand a)
-             | Jmp Label -- Jump to generic label
-             | Jsr Label -- Jump to subroutine (updates LR)
-             | Jl Label
-             | Jg Label
-             | Jle Label
-             | Jge Label
-             | Push (Operand a)
-             | Pop (Operand a)
-             | Comment T.Text -- Creates a comment in the assembly file
+data Instr a
+  = Load (Operand a) (Operand a)
+  | Store (Operand a) (Operand a)
+  | Mov (Operand a) (Operand a)
+  | Add (Operand a) (Operand a) (Operand a)
+  | Sub (Operand a) (Operand a) (Operand a)
+  | Mul (Operand a) (Operand a) (Operand a)
+  | Div (Operand a) (Operand a) (Operand a)
+  | Cmp (Operand a) (Operand a)
+  | Jmp Label -- Jump to generic label
+  | Jsr Label -- Jump to subroutine (updates LR)
+  | Jl Label
+  | Jg Label
+  | Jle Label
+  | Jge Label
+  | Push (Operand a)
+  | Pop (Operand a)
+  | Comment T.Text -- Creates a comment in the assembly file
 
 type Label = T.Text
 
-data Operand a = Reg a
-               | Regs [a] -- for Push and Pop
-               | Imm Int   
-               | Abs Label    
-               | Ind a  -- register indirect
+data Operand a
+  = Reg a
+  | Regs [a] -- for Push and Pop
+  | Imm Int
+  | Abs Label
+  | Ind a -- register indirect
 
 type FPOffsets = M.Map Ident Int
