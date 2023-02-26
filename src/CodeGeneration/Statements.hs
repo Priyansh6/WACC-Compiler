1317bfac391d975078468fc32f02a68710279300
Hamish McCreanor <hmccreanor1@tribeig.com>
2023-02-26 20:02:49 +0000

    feat: translating newpair statements

--- src/CodeGeneration/Statements.hs
+++ src/CodeGeneration/Statements.hs
@@ -4,37 +4,37 @@ module CodeGeneration.Statements (transStats) where
 import AST hiding (Ident)
 import CodeGeneration.IR
 import CodeGeneration.Expressions (transExp)
-import CodeGeneration.Utils (IRStatementGenerator, nextFreeReg, makeRegAvailable, insertVarReg, getVarReg, nextLabel)
+import CodeGeneration.Utils (IRStatementGenerator, (<++), nextFreeReg, makeRegAvailable, insertVarReg, getVarReg, nextLabel, exprType, typeSize, makeRegsAvailable)
 
-import qualified AST (Ident(Ident)) 
+import qualified AST (Ident(Ident))
 
 transStats :: Stats -> IRStatementGenerator IRInstrs
-transStats ss = concat <$> mapM transStat ss 
+transStats ss = concat <$> mapM transStat ss
 
 transStat :: Stat -> IRStatementGenerator IRInstrs
 transStat Skip = return []
 transStat (DecAssign t (AST.Ident i _) r _) = do
   varReg <- nextFreeReg
   rReg <- nextFreeReg
-  rInstrs <- transRVal r rReg 
+  rInstrs <- transRVal r rReg
   makeRegAvailable rReg
   insertVarReg (Ident i) varReg
   return $ rInstrs ++ [Mov (Reg varReg) (Reg rReg)]
 transStat (Assign (LIdent (AST.Ident i _)) r _) = do
   varReg <- getVarReg (Ident i)
   rReg <- nextFreeReg
-  rInstrs <- transRVal r rReg 
+  rInstrs <- transRVal r rReg
   makeRegAvailable rReg
   return $ rInstrs ++ [Mov (Reg varReg) (Reg rReg)]
 transStat (Assign _ _ _) = return []
 transStat (Read l _) = return []
 transStat (Free e _) = return []
 transStat (Return e _) = do
-  dst <- nextFreeReg 
+  dst <- nextFreeReg
   eis <- transExp e dst
   makeRegAvailable dst
   return $ eis ++ [Mov (Reg IRRet) (Reg dst)]
-transStat (Exit e _) = do 
+transStat (Exit e _) = do
   dst <- nextFreeReg
   eis <- transExp e dst
   makeRegAvailable dst
@@ -55,7 +55,7 @@ transStat (If e ss _ ss' _ _) = do
   makeRegAvailable eReg
   return $ eInstrs ++ condJumpInstrs ++ branchInstrs ++ branchInstrs' ++ [Define endLabel]
 transStat (While e ss _ _) = do
-  eReg <- nextFreeReg  
+  eReg <- nextFreeReg
   eInstrs <- transExp e eReg
   ssInstrs <- transStats ss
   startLabel <- nextLabel
@@ -68,7 +68,25 @@ transStat (Begin ss _) = transStats ss
 transRVal :: RVal -> IRReg -> IRStatementGenerator IRInstrs
 transRVal (RExpr e) dst = transExp e dst
 transRVal (ArrayLiter es _) dst = return []
-transRVal (NewPair e e' _) dst = return []
+transRVal (NewPair e e' _) dst = do
+  eReg <- nextFreeReg
+  ePtrReg <- nextFreeReg
+  eReg' <- nextFreeReg
+  ePtrReg' <- nextFreeReg
+  eType <- exprType e
+  eType' <- exprType e'
+  let pType = AST.WPair eType eType'
+      movePointers = [Mov (Reg dst) (Reg IRRet), Store (Reg eReg) (ImmOffset dst 0), Store (Reg eReg') (ImmOffset dst (typeSize eType))]
+  mallocFst <- transMallocCall $ typeSize eType
+  mallocSnd <- transMallocCall $ typeSize eType'
+  mallocPair <- transMallocCall $ typeSize pType
+  evalFstInstrs <- transExp e eReg <++ mallocFst ++ [Mov (Reg ePtrReg) (Reg IRRet), Mov (Ind ePtrReg) (Reg eReg)] -- not sure if Ind as the dst of a Mov is dodgy or not
+  evalSndInstrs <- transExp e' eReg' <++ mallocSnd ++ [Mov (Reg ePtrReg') (Reg IRRet), Mov (Ind ePtrReg') (Reg eReg')]
+  makeRegsAvailable [eReg, eReg', ePtrReg, ePtrReg']
+  return $ evalFstInstrs ++ evalSndInstrs ++ mallocPair ++ movePointers
+
 transRVal (RPair pe) dst = return []
 transRVal (Call (AST.Ident i _) es _) dst = return []
- 
+
+transMallocCall :: Int -> IRStatementGenerator IRInstrs
+transMallocCall size = return [Mov (Reg (IRParam 0)) (Imm size), Jsr "malloc"]
