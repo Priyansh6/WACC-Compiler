b1b033d2029ae9e676781ea682b72007d842d696
Priyansh Mahajan <pm521@ic.ac.uk>
2023-03-01 17:43:00 +0000

    feat: now allocates registers properly to arm registers or stack space.

--- app/Main.hs
+++ app/Main.hs
@@ -15,7 +15,6 @@ import Control.Monad.Except
 import Control.Monad.Reader
 import Control.Monad.Trans.State
 import qualified Data.Map as M
-import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import System.Environment
 import System.Exit
--- src/CodeGeneration/ARM/Registers.hs
+++ src/CodeGeneration/ARM/Registers.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module CodeGeneration.ARM.Registers (ArmInstr, ArmInstrs, ArmReg, transProg) where
 
 import CodeGeneration.IR
@@ -18,16 +20,18 @@ type ArmRegs = S.Set ArmReg
 type FPOffset = Int
 
 data Aux = Aux {
-  currFPOffset :: FPOffset,
+  nextFPOffset :: FPOffset,
+  scratchesAvailable :: ArmRegs,
   regsAvailable :: ArmRegs,
   regLocs :: M.Map IRReg (Either ArmReg FPOffset) }
 
 type ArmTranslator a = State Aux a
-type ArmMemoryAllocator a = WriterT (ArmInstrs, ArmInstrs) (State Aux) a
+type ArmMemoryAllocator a = WriterT (ArmInstrs, ArmInstrs, ArmRegs) (State Aux) a
 
 initAux :: Aux
 initAux = Aux {
-    currFPOffset = 0,
+    nextFPOffset = -4,
+    scratchesAvailable = scratchRegs,
     regsAvailable = generalRegs,
     regLocs = M.empty
   }
@@ -38,20 +42,11 @@ generalRegs = S.fromList [R0, R1, R2, R3, R4, R5, R6, R7]
 paramRegs :: ArmRegs
 paramRegs = S.fromList [R0, R1, R2, R3]
 
+scratchRegs :: ArmRegs
+scratchRegs = S.fromList [R8, R10, R12]
+
 retReg :: ArmReg
 retReg = R0
-
-scratch1 :: ArmReg 
-scratch1 = R8
-
-scratch2 :: ArmReg 
-scratch2 = R10
-
-scratch3 :: ArmReg 
-scratch3 = R12
-
-regsInUse :: ArmRegs -> ArmRegs
-regsInUse regsAvailable = generalRegs S.\\ regsAvailable
 
 transProg :: Program IRReg -> Program ArmReg
 transProg = map (flip evalState initAux . transSection)
@@ -62,21 +57,22 @@ transSection (Section d (Body label global instrs))
 
 transAndAddMemoryInstrs :: Instr IRReg -> ArmTranslator ArmInstrs
 transAndAddMemoryInstrs instr = do
-  (translatedInstr, (loadInstrs, storeInstrs)) <- runWriterT (transInstr instr)
-  return $ loadInstrs ++ [translatedInstr] ++ storeInstrs
+  (translatedInstr, (loadInstrs, storeInstrs, rs)) <- runWriterT (transInstr instr)
+  _ <- mapM makeScratchAvailable $ S.toList rs
+  return $ [Comment "start"] ++ loadInstrs ++ [translatedInstr] ++ storeInstrs ++ [Comment "end"]
 
 transInstr :: Instr IRReg -> ArmMemoryAllocator ArmInstr
-transInstr (Load o1 o2) = Load <$> transOperand o1 <*> transOperand o2
-transInstr (Store o1 o2) = Store <$> transOperand o1 <*> transOperand o2
-transInstr (Mov o1 o2) = Mov <$> transOperand o1 <*> transOperand o2
-transInstr (Add o1 o2 o3) = Add <$> transOperand o1 <*> transOperand o2 <*> transOperand o3
-transInstr (Sub o1 o2 o3) = Sub <$> transOperand o1 <*> transOperand o2 <*> transOperand o3
-transInstr (Mul o1 o2 o3) = Mul <$> transOperand o1 <*> transOperand o2 <*> transOperand o3
-transInstr (Div o1 o2 o3) = Div <$> transOperand o1 <*> transOperand o2 <*> transOperand o3
-transInstr (Cmp o1 o2) = Mov <$> transOperand o1 <*> transOperand o2
+transInstr (Load o1 o2) = Load <$> transOperand o1 False <*> transOperand o2 True
+transInstr (Store o1 o2) = Store <$> transOperand o1 True <*> transOperand o2 False
+transInstr (Mov o1 o2) = Mov <$> transOperand o1 True <*> transOperand o2 False 
+transInstr (Add o1 o2 o3) = Add <$> transOperand o1 True <*> transOperand o2 False <*> transOperand o3 False 
+transInstr (Sub o1 o2 o3) = Sub <$> transOperand o1 True <*> transOperand o2 False <*> transOperand o3 False 
+transInstr (Mul o1 o2 o3) = Mul <$> transOperand o1 True <*> transOperand o2 False <*> transOperand o3 False 
+transInstr (Div o1 o2 o3) = Div <$> transOperand o1 True <*> transOperand o2 False <*> transOperand o3 False
+transInstr (Cmp o1 o2) = Mov <$> transOperand o1 True <*> transOperand o2 False
 transInstr (Jsr l) = return $ Jsr l
-transInstr (Push o) = Push <$> transOperand o
-transInstr (Pop o) = Pop <$> transOperand o
+transInstr (Push o) = Push <$> transOperand o False
+transInstr (Pop o) = Pop <$> transOperand o False
 transInstr (Jmp l) = return $ Jmp l
 transInstr (Je l) = return $ Je l
 transInstr (Jne l) = return $ Jne l
@@ -87,22 +83,65 @@ transInstr (Jge l) = return $ Jge l
 transInstr (Define l) = return $ Define l
 transInstr (Comment c) = return $ Comment c
 
-transOperand :: Operand IRReg -> ArmMemoryAllocator (Operand ArmReg)
-transOperand (Reg r) = Reg <$> transIRReg r
-transOperand (Regs rs) = Regs <$> mapM transIRReg rs
-transOperand (Ind r) = Ind <$> transIRReg r
-transOperand (ImmOffset r offset) = flip ImmOffset offset <$> transIRReg r
-transOperand (Imm i) = return $ Imm i
-transOperand (Abs l) = return $ Abs l
+transOperand :: Operand IRReg -> Bool -> ArmMemoryAllocator (Operand ArmReg)
+transOperand (Reg r) isDst = Reg <$> transIRReg r isDst
+transOperand (Regs rs) isDst = Regs <$> mapM (flip transIRReg isDst) rs
+transOperand (Ind r) isDst = Ind <$> transIRReg r isDst
+transOperand (ImmOffset r offset) isDst = flip ImmOffset offset <$> transIRReg r isDst
+transOperand (Imm i) _ = return $ Imm i
+transOperand (Abs l) _ = return $ Abs l
 
-transIRReg :: IRReg -> ArmMemoryAllocator ArmReg
-transIRReg tr@(TmpReg _) = return R0
-transIRReg pr@(IRParam _) = return R0
-transIRReg IRScratch1 = return scratch1
-transIRReg IRScratch2 = return scratch2
-transIRReg IRScratch3 = return scratch3
-transIRReg IRFP = return FP
-transIRReg IRSP = return SP
-transIRReg IRLR = return LR
-transIRReg IRPC = return PC
-transIRReg IRRet = return retReg
+transIRReg :: IRReg -> Bool -> ArmMemoryAllocator ArmReg
+transIRReg tr@(TmpReg _) isDst = allocateRegLoc tr generalRegs >> transDynamicReg tr isDst
+transIRReg pr@(IRParam _) isDst = allocateRegLoc pr paramRegs >> transDynamicReg pr isDst
+transIRReg IRScratch1 _ = return $ S.elemAt 0 scratchRegs
+transIRReg IRScratch2 _ = return $ S.elemAt 1 scratchRegs
+transIRReg IRScratch3 _ = return $ S.elemAt 2 scratchRegs
+transIRReg IRFP _ = return FP
+transIRReg IRSP _ = return SP
+transIRReg IRLR _ = return LR 
+transIRReg IRPC _ = return PC
+transIRReg IRRet _ = return retReg
+
+transDynamicReg :: IRReg -> Bool -> ArmMemoryAllocator ArmReg
+transDynamicReg r isDst = do
+  locs <- gets regLocs
+  case (M.lookup r locs) of
+    Nothing -> error("Can't translate register if it hasn't been allocated a location.")
+    Just (Left armReg) -> return armReg
+    Just (Right fpOff) -> do
+      sr <- getFreeScratch
+      tell ([],[], S.fromList [sr])
+      tell $ if isDst
+        then ([], [Store (Reg sr) (ImmOffset FP fpOff)], S.empty)
+        else ([Load (Reg sr) (ImmOffset FP fpOff)], [], S.empty)
+      return sr
+
+allocateRegLoc :: IRReg -> ArmRegs -> ArmMemoryAllocator ()
+allocateRegLoc r regOptions = do
+  locs <- gets regLocs
+  case (M.lookup r locs) of
+    Nothing -> do
+      loc <- getFreeLoc regOptions
+      modify (\a -> a {regLocs = M.insert r loc locs})
+    _ -> return ()
+
+getFreeLoc :: ArmRegs -> ArmMemoryAllocator (Either ArmReg FPOffset)
+getFreeLoc regOptions = do
+  rs <- gets regsAvailable
+  let nextRs = regOptions `S.intersection` rs
+  if null nextRs
+    then do
+      fpOff <- gets nextFPOffset
+      modify (\a -> a {nextFPOffset = fpOff - 4})
+      return $ Right fpOff
+    else do
+      let nextR = S.elemAt 0 nextRs
+      modify (\a -> a {regsAvailable = S.delete nextR rs})
+      return $ Left nextR
+
+getFreeScratch :: ArmMemoryAllocator ArmReg
+getFreeScratch = state (\a@Aux {scratchesAvailable = rs} -> let r = S.elemAt 0 rs in (r, a {scratchesAvailable = S.delete r rs}))
+
+makeScratchAvailable :: ArmReg -> ArmTranslator ()
+makeScratchAvailable r = modify (\a@Aux {scratchesAvailable = rs} -> a {scratchesAvailable = S.insert r rs})
