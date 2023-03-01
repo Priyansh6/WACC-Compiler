08aac5e52ddb942478ad7ec3fc2de6960b952e3a
Hamish McCreanor <hwm21@ic.ac.uk>
2023-03-01 20:47:42 +0000

    feat: generating helper functions identified via transStats

--- src/CodeGeneration/Program.hs
+++ src/CodeGeneration/Program.hs
@@ -5,6 +5,7 @@ module CodeGeneration.Program (transProg) where
 import CodeGeneration.IR 
 import CodeGeneration.Statements (transStats)
 import CodeGeneration.DataSection
+import CodeGeneration.Helpers (generateHelperFuncs, HelperFuncs)
 import CodeGeneration.Utils (IRSectionGenerator, Aux(..))
 import Control.Monad.State
 
@@ -14,28 +15,28 @@ import qualified Data.Set as S
 
 transProg :: AST.Program -> IRSectionGenerator (Program IRReg)
 transProg (AST.Program fs ss) = do
-  mainSection <- transMain ss
-  otherSections <- mapM transFunc fs
-  return $ otherSections ++ [mainSection]
+  (mainSection, hfs) <- transMain ss
+  (otherSections, hfs') <- unzip <$> mapM transFunc fs
+  return $ otherSections ++ [mainSection] ++ generateHelperFuncs (S.union hfs (S.unions hfs'))
 
-transMain :: AST.Stats -> IRSectionGenerator (Section IRReg)
+transMain :: AST.Stats -> IRSectionGenerator (Section IRReg, HelperFuncs)
 transMain ss = do 
   let unlimitedRegs = map TmpReg [0..]
       name = "main"
       (dataSection, lt) = generateDataSection ss name
-  bodyInstrs <- evalStateT (transStats ss) (Aux {available = unlimitedRegs, labelId = 0, varLocs = M.empty, sectionName = name, literTable = lt, helperFuncs = S.empty, inUse = []}) 
+  (bodyInstrs, aux) <- runStateT (transStats ss) (Aux {available = unlimitedRegs, labelId = 0, varLocs = M.empty, sectionName = name, literTable = lt, helperFuncs = S.empty, inUse = []}) 
   case bodyInstrs of 
-    [] -> return $ Section dataSection (Body name True (wrapSectionBody [Mov (Reg IRRet) (Imm 0)]))
-    _ -> return $ Section dataSection (Body name True (wrapSectionBody bodyInstrs))
+    [] -> return (Section dataSection (Body name True (wrapSectionBody [Mov (Reg IRRet) (Imm 0)])), helperFuncs aux)
+    _ -> return (Section dataSection (Body name True (wrapSectionBody bodyInstrs)), helperFuncs aux)
 
-transFunc :: AST.Func -> IRSectionGenerator (Section IRReg)
+transFunc :: AST.Func -> IRSectionGenerator (Section IRReg, HelperFuncs)
 transFunc (AST.Func _ (AST.Ident i _) params ss _ _) = do
   let unlimitedRegs = map TmpReg [0..]
       name = i
       (dataSection, lt) = generateDataSection ss name
       pushParamInstrs = [Mov (Reg (TmpReg x)) (Reg (IRParam x)) | x <- [0 .. (length params)]]
-  bodyInstrs <- evalStateT (transStats ss) (Aux {available = unlimitedRegs, labelId = 0, varLocs = M.empty, sectionName = name, literTable = lt, helperFuncs = S.empty, inUse = []})
-  return $ Section dataSection (Body name True (wrapSectionBody (pushParamInstrs ++ bodyInstrs)))
+  (bodyInstrs, aux) <- runStateT (transStats ss) (Aux {available = unlimitedRegs, labelId = 0, varLocs = M.empty, sectionName = name, literTable = lt, helperFuncs = S.empty, inUse = []})
+  return (Section dataSection (Body name True (wrapSectionBody (pushParamInstrs ++ bodyInstrs))), helperFuncs aux)
 
 wrapSectionBody :: IRInstrs -> IRInstrs
 wrapSectionBody ss = [Push (Regs [IRFP, IRLR]), Mov (Reg IRFP) (Reg IRSP)] ++ ss ++ [Pop (Regs [IRFP, IRPC])]
