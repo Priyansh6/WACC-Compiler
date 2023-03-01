c9f8ef4b2c2b2cbd13e9823c38294a67c93aaa87
Hamish McCreanor <hwm21@ic.ac.uk>
2023-03-01 20:07:52 +0000

    fix: fixed translation of negation operator

--- src/CodeGeneration/Expressions.hs
+++ src/CodeGeneration/Expressions.hs
@@ -52,7 +52,7 @@ transExp (Not e _) dst = do
   return $ exprInstrs ++ [Cmp (Reg dst) (Imm 1), Je trueLabel, Mov (Reg eReg) (Imm 1), Jmp endLabel, Define trueLabel, Mov (Reg eReg) (Imm 0), Define endLabel, Mov (Reg dst) (Reg eReg)]
 transExp (Neg e _) dst = do
   eReg <- nextFreeReg
-  exprInstrs <- transExp e dst
+  exprInstrs <- transExp e eReg
   makeRegAvailable eReg
   return $ exprInstrs ++ [Mov (Reg dst) (Imm 0), Sub (Reg dst) (Reg dst) (Reg eReg)]
 transExp (Len e _) dst = transExp e dst <++ [Load (Reg dst) (ImmOffset dst (- (typeSize WInt)))]
